1ce323b778efbc8fc4cdf6893ac7d800ea240982
allankalema4 <allankalema4@gmail.com>
2024-11-15 20:20:23 +0300

    rectified retrieval in the models

--- properties/models.py
+++ properties/models.py
@@ -40,7 +40,7 @@ class LandProperty(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.property_id} - {self.location}"
+        return f"{self.name} - {self.location}"
 
 
 class OwnershipTransfer(models.Model):
