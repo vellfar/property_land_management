d2bdb8b3c9561b883b9d11fff1bbc76b0ffa8ecd
fearless-programmer <trevorssemwanga@gmail.com>
2024-11-16 15:25:22 +0300

    Corrections

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from properties.models import OwnershipTransfer
 
 # Create your views here.
 def index(request):
     return render(request, 'properties/index.html')
 
 def transfers(request):
-    return render(request, 'properties/transfers.html')
+    user_id = request.user.id
+    property_transfers = OwnershipTransfer.objects.filter(current_owner=user_id).filter(status=1 or 2)
+    context = {
+        'transfers': property_transfers
+    }
+    return render(request, 'properties/transfers.html', context)
